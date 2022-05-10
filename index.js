3858f096215dc9d45e5c1121d3f79e829b4abd06
zackmolt <fkrihkl@gmail.com>
2022-05-11 00:18:00 +0700

    set cors and set env config

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || envDummy.PORT;
 
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser()); 
+app.use(cors(corsOptions));
 
 const router = require('./routes/router.js'); 
 
